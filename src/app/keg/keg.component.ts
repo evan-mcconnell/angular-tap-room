df4929fd82dc350c8cca951fcb015d3f1ccd29c2
Kimberly McConnell and Matt Groberg <kimberly@>
2019-03-27 15:59:06 -0700

    add fullness bar for keg fullness and pics for each drink type

--- src/app/keg/keg.component.ts
+++ src/app/keg/keg.component.ts
@@ -13,8 +13,32 @@ export class KegComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    console.log(this.data);
   }
 
-  
-
+  sellPint(){
+    this.pints--;
+  }
+  displayFill(){
+    if (this.pints===124){
+      return 'new-keg';
+    } else if(this.pints>=124*0.75){
+      return 'full-keg';
+    } else if(this.pints>=124*0.25){
+      return 'med-keg';
+    } else if (this.pints>=124*0.05){
+      return 'low-keg';
+    } else {
+      return 'empty-keg';
+    }
+  }
+  drinkIcon() {
+    if (this.data.type === "IPA") {
+      return '../assets/hop.jpeg';
+    } else if (this.data.type === "Stout") {
+      return '../assets/stout.jpg';
+    } else if (this.data.type === "Kombucha") {
+      return '../assets/Kombucha.jpg';
+    }
+  }
 }
